80a7d6e5ca0c6ee71aa7f6cb3d6ae2419b49ec10
Lê Minh Trí <20120600@student.hcmus.edu.vn>
2025-09-09 16:42:30 +0700

    Add the build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+mkdir -p "build/"
+
+# Build the Go server
+# cd be/
+# echo "Building server"
+# go build -o ../build/server ./cmd/web/* 
+# echo "Build complete!"
+# cd ..
+
+# Build the React server
+cd fe/
+echo "Build frontend"
+npm install
+npm run build
+echo "Build frontend complete!"
